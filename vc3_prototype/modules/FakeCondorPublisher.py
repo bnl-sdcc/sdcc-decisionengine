7fbeec80e939e4600e83deb4456c2c14a221a024
Jose Caballero <jcaballero@bnl.gov>
2018-06-25 17:20:35 -0400

    updated

--- vc3_prototype/modules/FakeCondorPublisher.py
+++ vc3_prototype/modules/FakeCondorPublisher.py
@@ -1,41 +1,6 @@
 from decisionengine.framework.modules import Module
 from decisionengine.framework.modules import de_logger
 from decisionengine.framework.modules import Publisher
-import abc
-
-
-
-#class FakeCondorPublisher(Module.Module):
-#    def __init__(self, set_of_parameters):
-#        Module.Module.__init__(self, set_of_parameters)
-#        self.logger = de_logger.get_logger()
-#        self.logger.info('__init__ completed')
-#
-
-#class GenericFakeCondorPublisher(Publisher.Publisher):
-#
-#    __metaclass__ = abc.ABCMeta
-#
-#    def __init__(self, *args, **kwargs):
-#        #super(FakeCondorPublisher, self).__init__(*args, **kwargs)
-#        self.logger = de_logger.get_logger()
-#        self.logger.info('__init__ completed')
-#
-#    @abc.abstractmethod
-#    def consumes(self, name_list):
-#	return None
-#
-#    def publish(self, data_block=None):
-#	self.logger.debug('>>> starting publish()')
-#
-#
-#class FakeCondorPublisher(GenericFakeCondorPublisher):
-#	def __init__(self, *k, **kw):
-#		super(FakeCondorPublisher, self).__init__(*k, **kw)
-#
-#	def consumes(self):
-#		self.logger.debug('>>> starting consumes()')
-
 
 class FakeCondorPublisher(Publisher.Publisher):
     def __init__(self, *args, **kwargs):
@@ -48,3 +13,57 @@ class FakeCondorPublisher(Publisher.Publisher):
     def publish(self, data_block=None):
         self.logger.info('>>> calling publish with data_block=%s' %data_block)
         #self.logger.info('>>> number of new glideins : %s' %data_block['n_glideins'][0][0])
+        self.logger.info('>>> number of new glideins : %s' %data_block['n_glideins'])
+
+    vc3 = VC3()
+    vc3.list()
+
+
+# -----------------------------------------------------------------------------
+
+from vc3client.client import VC3ClientAPI
+from vc3infoservice.infoclient import InfoClient
+import datetime
+from ConfigParser import SafeConfigParser
+import os
+
+class VC3(object):
+
+    def __init__(self):
+
+    self.log = de_logger.get_logger()
+        self.config = SafeConfigParser()
+        self.config.readfp(open('/etc/vc3/vc3-client.conf'))
+        self.certfile  = os.path.expanduser(self.config.get('netcomm','certfile'))
+        self.keyfile   = os.path.expanduser(self.config.get('netcomm', 'keyfile'))
+        self.chainfile = os.path.expanduser(self.config.get('netcomm','chainfile'))
+    self.client = VC3ClientAPI(self.config)     
+
+
+    def list(self):
+
+    self.log.debug('list of resources = %s' %self.client.listResources())       
+
+
+    def request(self):
+
+
+        now = datetime.datetime.utcnow()
+        t_delta = datetime.timedelta(hours=1)
+        expiration = now + t_delta
+        expiration = expiration.replace(microsecond=0).isoformat()
+
+
+        request = self.client.defineRequest("hepcloud", 
+                                            "jcaballero", 
+                                            "jcaballero-hepcloud", 
+                                            ["jcaballero.vc3-test-pool"], 
+                                            ["jcaballero-hepcloud"],
+                                            "static-balanced",
+                                            expiration,
+                                            "hepcloud")
+
+        self.client.storeRequest(request, "jcaballero")
+
+
+
